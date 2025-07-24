84db395c6be43891de9b1a957db06e5362434951
Gcrin <94522022+Gcrin@users.noreply.github.com>
2025-07-24 21:17:14 +0900

    feat(AbilitySystem): GameplayAbilities 기반 클래스 및 AttributeSet 추가
    GameplayAbilities 확장 기능 구현을 위한 기반 클래스 생성.
    
    - `DrgGameplayAbility`와 `DrgAttributeSet` 클래스 추가
    - `Drg.uproject`에 GameplayAbilities 메커니즘 활성화를 위한 의존성 설정 추가

---
+++ Source/Drg/AbilitySystem/Attributes/DrgAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "DrgAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRG_API UDrgAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
