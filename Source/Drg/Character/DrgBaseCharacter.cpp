0b5de782bc56d6eadb603e45f2b2cc743ef9a70f
Gcrin <94522022+Gcrin@users.noreply.github.com>
2025-07-24 21:18:18 +0900

    feat(Character): 기본 캐릭터 및 플레이어 컨트롤러 클래스 추가
    게임 캐릭터와 플레이어 컨트롤러의 기본 클래스를 생성하여 기능 확장을 위한 기반을 마련함.
    
    - `ADrgBaseCharacter` 클래스 생성
      - `ACharacter`를 상속받아 기본 초기화 메서드 및 `BeginPlay` 함수 구현
    - `ADrgPlayerController` 클래스 생성
      - `APlayerController`를 상속받은 빈 클래스 추가

---
+++ Source/Drg/Character/DrgBaseCharacter.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DrgBaseCharacter.h"
+
+ADrgBaseCharacter::ADrgBaseCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ADrgBaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/Drg/Character/DrgBaseCharacter.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "DrgBaseCharacter.generated.h"
+
+UCLASS()
+class DRG_API ADrgBaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ADrgBaseCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+};
