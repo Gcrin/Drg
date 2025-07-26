f82ad3b9f70249f5c81d66e681ead2d2b219bf82
Gcrin <94522022+Gcrin@users.noreply.github.com>
2025-07-26 12:37:04 +0900

    feat(AbilitySystem): DrgAbilitySystemComponent 추가
    Ability System을 확장하기 위해 DrgAbilitySystemComponent 클래스를 생성함.

---
+++ Source/Drg/AbilitySystem/DrgAbilitySystemComponent.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "DrgAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRG_API UDrgAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+};
