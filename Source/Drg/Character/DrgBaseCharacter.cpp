b1eeff6e11b8ebdb97d1852f2f2af9df090617bd
Gcrin <94522022+Gcrin@users.noreply.github.com>
2025-07-26 12:44:16 +0900

    feat(Character): Ability System 추가 및 초기화 로직 구현
    DrgBaseCharacter에 Ability System을 통합하고 초기화 로직을 구현함.
    
    - `DrgBaseCharacter`에 `UDrgAbilitySystemComponent`와 `UDrgAttributeSet` 프로퍼티 추가
    - `IAbilitySystemInterface`를 구현하여 Ability System과의 호환성 확보
    - 서버 및 클라이언트에 대한 Ability System 초기화 로직 구현 (`PossessedBy` 및 `OnRep_PlayerState` 메서드)
    - 기본 어트리뷰트와 능력을 초기화하고 부여하는 로직 추가 (`InitializeAttributes`, `GrantAbilities` 메서드)
    - `DefaultAttributes` 및 `DefaultAbilities`를 통해 초기화 데이터 구성

--- Source/Drg/Character/DrgBaseCharacter.cpp
+++ Source/Drg/Character/DrgBaseCharacter.cpp
@@ -4,7 +4,10 @@
 #include "DrgBaseCharacter.h"
 
 #include "Camera/CameraComponent.h"
+#include "Drg/AbilitySystem/DrgAbilitySystemComponent.h"
+#include "Drg/AbilitySystem/Attributes/DrgAttributeSet.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/PlayerState.h"
 #include "GameFramework/SpringArmComponent.h"
 
 ADrgBaseCharacter::ADrgBaseCharacter()
@@ -29,13 +32,72 @@ ADrgBaseCharacter::ADrgBaseCharacter()
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
 	bUseControllerRotationRoll = false;
-	
+
 	GetCharacterMovement()->bOrientRotationToMovement = true;
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
-	
+
+	AbilitySystemComponent = CreateDefaultSubobject<UDrgAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AttributeSet = CreateDefaultSubobject<UDrgAttributeSet>(TEXT("AttributeSet"));
+}
+
+UAbilitySystemComponent* ADrgBaseCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
 }
 
 void ADrgBaseCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+}
+
+void ADrgBaseCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	// 서버에서 GAS 초기화
+	InitializeAttributes();
+}
+
+void ADrgBaseCharacter::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+
+	// 클라이언트에서 GAS 초기화
+	InitializeAttributes();
+}
+
+void ADrgBaseCharacter::InitializeAttributes()
+{
+	if (AbilitySystemComponent && GetPlayerState())
+	{
+		AbilitySystemComponent->InitAbilityActorInfo(GetPlayerState(), this);
+
+		check(DefaultAttributes);
+
+		FGameplayEffectContextHandle EffectContext = AbilitySystemComponent->MakeEffectContext();
+		EffectContext.AddSourceObject(this);
+
+		FGameplayEffectSpecHandle SpecHandle = AbilitySystemComponent->MakeOutgoingSpec(
+			DefaultAttributes, 1, EffectContext);
+
+		if (SpecHandle.IsValid())
+		{
+			AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+		}
+
+		GrantAbilities();
+	}
+}
+
+void ADrgBaseCharacter::GrantAbilities()
+{
+	if (!HasAuthority() || !AbilitySystemComponent)
+	{
+		return;
+	}
+
+	for (TSubclassOf<UGameplayAbility>& Ability : DefaultAbilities)
+	{
+		AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec(Ability));
+	}
 }
--- Source/Drg/Character/DrgBaseCharacter.h
+++ Source/Drg/Character/DrgBaseCharacter.h
@@ -3,19 +3,40 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
 #include "GameFramework/Character.h"
 #include "DrgBaseCharacter.generated.h"
 
+class UDrgAttributeSet;
+class UDrgAbilitySystemComponent;
+
 UCLASS()
-class DRG_API ADrgBaseCharacter : public ACharacter
+class DRG_API ADrgBaseCharacter : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
 	ADrgBaseCharacter();
 
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
 protected:
 	virtual void BeginPlay() override;
+
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_PlayerState() override;
+	// GAS 초기화
+	virtual void InitializeAttributes();
+	virtual void GrantAbilities();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Drg|AbilitySystem")
+	TObjectPtr<UDrgAbilitySystemComponent> AbilitySystemComponent;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Drg|AbilitySystem")
+	TObjectPtr<UDrgAttributeSet> AttributeSet;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Drg|AbilitySystem")
+	TSubclassOf<class UGameplayEffect> DefaultAttributes;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Drg|AbilitySystem")
+	TArray<TSubclassOf<class UGameplayAbility>> DefaultAbilities;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Drg|Camera")
 	TObjectPtr<class USpringArmComponent> SpringArmComponent;
