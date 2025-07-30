b2237e84d659bc3d2c25a7c44c85ff923a20b17f
SKim041 <sehee1806@gmail.com>
2025-07-30 20:02:07 +0900

    feat(AI): Timer로 스폰 반복하는 기능 추가
    - StartSpawnTimer: 현재 웨이브 정보로 타이머 실행
    - StopSpawnTimer: 타이머 clear
    - SpawnAILoop: 현재 최대스폰카운트 초과하는지 판단
    - 블루프린트에서 안쓰는 함수 UFUNCTION 매크로 삭제
    - CurrentWaveNumber, CurrentWaveRow, SpawnTimerHandle, CurrentSpawnCount 변수 추가

--- Source/Drg/AI/DrgSpawnAI.h
+++ Source/Drg/AI/DrgSpawnAI.h
@@ -34,23 +34,32 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Drg|Spawning")
 	TArray<class ADrgBaseCharacter*> InActiveAIPool;
 	int32 MaxTryCount = 30;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Drg|Spawning")
+	int32 CurrentWaveNumber;
 
+	struct FDrgWaveTableRow* CurrentWaveRow;
 	FTimerHandle SpawnTimerHandle;
+	int32 CurrentSpawnCount = 0;
 
 	// 레벨 시작 시 AI 캐릭터 미리 스폰해서 InActiveAIPool에 추가
 	// 이 이상 AI 캐릭터 스폰하지 않음
 	UFUNCTION(BlueprintCallable, Category = "Drg|Spawning")
 	void InitializePool();
+	// 현재 웨이브 스폰 타이머 시작. SpawnAILoop 반복 실행
+	UFUNCTION(BlueprintCallable, Category = "Drg|Spawning")
+	void StartSpawnTimer(int32 NewWaveNumber);
+	// 현재 웨이브 스폰 타이머 종료
+	UFUNCTION(BlueprintCallable, Category = "Drg|Spawning")
+	void StopSpawnTimer();
 
 	// Nav Mesh 내에 안전한 스폰 위치 
-	UFUNCTION(BlueprintCallable, Category = "Drg|Spawning")
 	bool FindSafeRandomPointInNav(FVector& ResultLocation);
 	// 데이터테이블 내 현재 웨이브 데이터 행 반환
-	UFUNCTION(BlueprintCallable, Category = "Drg|Spawning")
-	struct FDrgWaveTableRow GetCurrentWaveDataRow(int32 WaveNumber);
+	struct FDrgWaveTableRow* GetCurrentWaveDataRow(int32 WaveNumber);
 	// 현재 웨이브에 해당하는 AI 데이터에셋 랜덤하게 반환
-	UFUNCTION(BlueprintCallable, Category = "Drg|Spawning")
-	class UDrgCharacterData* GetRandomAICharacterData(FDrgWaveTableRow CurrentWaveRow);
-	UFUNCTION(BlueprintCallable, Category = "Drg|Spawning")
-	void SpawnAIFromPool(int32 WaveNumber);
+	TObjectPtr<class UDrgCharacterData> GetRandomAICharacterData();
+	// 데이터에셋 적용하여 안전한 위치에 스폰
+	class ADrgBaseCharacter* SpawnAIFromPool();
+	// 스폰 후 ActiveAIPool에 추가. 타이머에서 실행될 함수
+	void SpawnAILoop();
 };
