99e3fb57e42771e0dd18b7954b32c58c313f26aa
SKim041 <sehee1806@gmail.com>
2025-07-30 20:02:07 +0900

    feat(AI): Timer로 스폰 반복하는 기능 추가
    - StartSpawnTimer: 현재 웨이브 정보로 타이머 실행
    - StopSpawnTimer: 타이머 clear
    - SpawnAILoop: 현재 최대스폰카운트 초과하는지 판단
    - 블루프린트에서 안쓰는 함수 UFUNCTION 매크로 삭제
    - CurrentWaveNumber, CurrentWaveRow, SpawnTimerHandle, CurrentSpawnCount 변수 추가

--- Source/Drg/AI/DrgSpawnAI.cpp
+++ Source/Drg/AI/DrgSpawnAI.cpp
@@ -3,9 +3,9 @@
 #include "DrgSpawnAI.h"
 #include "NavigationSystem.h"
 #include "DrgWaveTableRow.h"
+#include "Components/CapsuleComponent.h"
 #include "Drg/Character/DrgBaseCharacter.h"
 #include "Drg/Character/Data/DrgCharacterData.h"
-#include "EntitySystem/MovieSceneEntitySystemRunner.h"
 #include "Kismet/GameplayStatics.h"
 
 // Sets default values
@@ -36,10 +36,37 @@ void ADrgSpawnAI::InitializePool()
 	}
 }
 
+void ADrgSpawnAI::StartSpawnTimer(int32 NewWaveNumber)
+{
+	CurrentWaveNumber = NewWaveNumber;
+
+	CurrentWaveRow = GetCurrentWaveDataRow(CurrentWaveNumber);
+	if (!CurrentWaveRow) return;
+
+	GetWorldTimerManager().SetTimer(
+		SpawnTimerHandle,
+		this,
+		&ADrgSpawnAI::SpawnAILoop,
+		CurrentWaveRow->SpawnInterval,
+		true,
+		1.0f
+	);
+	UE_LOG(LogTemp, Warning,
+	       TEXT("DrgSpawnAI:: %d 웨이브 시작"), CurrentWaveNumber);
+}
+
+void ADrgSpawnAI::StopSpawnTimer()
+{
+	GetWorldTimerManager().ClearTimer(SpawnTimerHandle);
+}
+
 bool ADrgSpawnAI::FindSafeRandomPointInNav(FVector& ResultLocation)
 {
 	UNavigationSystemV1* NavSys = UNavigationSystemV1::GetCurrent(GetWorld());
 	if (!NavSys) return false;
+	ACharacter* PlayerCharacter = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+	if (!PlayerCharacter) return false;
+	FVector PlayerLocation = PlayerCharacter->GetActorLocation();
 
 	for (int32 i = 0; i < MaxTryCount; i++)
 	{
@@ -51,56 +78,77 @@ bool ADrgSpawnAI::FindSafeRandomPointInNav(FVector& ResultLocation)
 			0.f
 		);
 
-		FVector TestLocation = GetActorLocation() + RandomOffset;
+		FVector TestLocation = PlayerLocation + RandomOffset;
 		FNavLocation NavLocation;
-		bool bIsFound = NavSys->ProjectPointToNavigation(TestLocation, NavLocation);
-		if (!bIsFound) continue;
+		if (NavSys->ProjectPointToNavigation(TestLocation, NavLocation))
+		{
+			FVector Start = NavLocation.Location + FVector(0, 0, 1000.0f);
+			FVector End = NavLocation.Location - FVector(0, 0, 1000.0f);
 
-		ResultLocation = NavLocation.Location;
+			FHitResult HitResult;
+			FCollisionQueryParams QueryParams;
+			QueryParams.bTraceComplex = true;
+			QueryParams.bReturnPhysicalMaterial = false;
+
+			bool bHit = GetWorld()->LineTraceSingleByChannel(
+				HitResult,
+				Start,
+				End,
+				ECC_Visibility,
+				QueryParams
+			);
+
+			if (bHit && HitResult.bBlockingHit)
+			{
+				ResultLocation = HitResult.Location;
+				return true;
+			}
+		}
 		return true;
 	}
 	return false;
 }
 
-struct FDrgWaveTableRow ADrgSpawnAI::GetCurrentWaveDataRow(int32 WaveNumber)
+FDrgWaveTableRow* ADrgSpawnAI::GetCurrentWaveDataRow(int32 WaveNumber)
 {
-	if (!WaveDataTable) return FDrgWaveTableRow{};
+	if (!WaveDataTable) return nullptr;
 
 	TArray<FDrgWaveTableRow*> AllRows;
 	static const FString ContextString(TEXT("GetRandomAICharacterDataContext"));
 	WaveDataTable->GetAllRows(ContextString, AllRows);
 
-	if (AllRows.Num() == 0) return FDrgWaveTableRow{};
+	if (AllRows.Num() == 0) return nullptr;
 
-	FDrgWaveTableRow* CurrentWaveRow = nullptr;
+	FDrgWaveTableRow* CurrentRow = nullptr;
 	for (FDrgWaveTableRow* Row : AllRows)
 	{
 		if (Row && Row->WaveNumber == WaveNumber)
 		{
-			CurrentWaveRow = Row;
+			CurrentRow = Row;
 			break;
 		}
 	}
-	if (!CurrentWaveRow)
+	if (!CurrentRow)
 	{
 		UE_LOG(LogTemp, Error,
 		       TEXT("DrgSpawnAI::WaveNumber %d에 해당하는 열을 찾을 수 없습니다. [해결 방법: DT_Wave에 해당 웨이브 정보를 입력해주세요]"), WaveNumber);
-		return FDrgWaveTableRow{};
+		check(CurrentRow!=nullptr);
+		return nullptr;
 	}
-	return *CurrentWaveRow;
+	return CurrentRow;
 }
 
-UDrgCharacterData* ADrgSpawnAI::GetRandomAICharacterData(FDrgWaveTableRow CurrentWaveRow)
+TObjectPtr<UDrgCharacterData> ADrgSpawnAI::GetRandomAICharacterData()
 {
 	float TotalChance = 0;
-	for (auto& p : CurrentWaveRow.AIData)
+	for (auto& p : CurrentWaveRow->AIData)
 	{
 		TotalChance += p.Value;
 	}
 
 	const float RandomValue = FMath::FRandRange(0.f, TotalChance);
 	float AccumulatedChance = 0;
-	for (auto& p : CurrentWaveRow.AIData)
+	for (auto& p : CurrentWaveRow->AIData)
 	{
 		AccumulatedChance += p.Value;
 		if (RandomValue <= AccumulatedChance)
@@ -112,10 +160,8 @@ UDrgCharacterData* ADrgSpawnAI::GetRandomAICharacterData(FDrgWaveTableRow Curren
 	return nullptr;
 }
 
-void ADrgSpawnAI::SpawnAIFromPool(int32 WaveNumber)
+ADrgBaseCharacter* ADrgSpawnAI::SpawnAIFromPool()
 {
-	FDrgWaveTableRow CurrentWaveRow = GetCurrentWaveDataRow(WaveNumber);
-	if (!CurrentWaveRow.AIData.Num()) return;
 	FVector SpawnLocation;
 	if (FindSafeRandomPointInNav(SpawnLocation))
 	{
@@ -124,12 +170,56 @@ void ADrgSpawnAI::SpawnAIFromPool(int32 WaveNumber)
 			AICharacterClass,
 			SpawnTransform
 		);
-		SpawnedAI->SetCharacterData(GetRandomAICharacterData(CurrentWaveRow));
+		SpawnedAI->SetCharacterData(GetRandomAICharacterData());
 		UGameplayStatics::FinishSpawningActor(SpawnedAI, SpawnTransform);
+		SpawnedAI->SetActorLocation(
+			SpawnedAI->GetActorLocation() + FVector(
+				0, 0, SpawnedAI->GetCapsuleComponent()->GetScaledCapsuleHalfHeight()));
+		return SpawnedAI;
 	}
 	else
 	{
-		UE_LOG(LogTemp, Error,
-		       TEXT("DrgSpawnAI::SpawnLocation을 찾을 수 없습니다."));
+		UE_LOG(LogTemp, Warning,
+		       TEXT("DrgSpawnAI::SpawnLocation을 찾을 수 없어 AI 스폰을 실패했습니다."));
+		return nullptr;
+	}
+}
+
+void ADrgSpawnAI::SpawnAILoop()
+{
+	if (ActiveAIPool.Num() >= TotalSpawnCount)
+	{
+		UE_LOG(LogTemp, Warning,
+		       TEXT("DrgSpawnAI::스폰된 AI가 TotalSpawnCount를 초과했습니다 %d. 더이상 스폰되지 않습니다."), ActiveAIPool.Num());
+		return;
+	}
+	for (int32 i = 0; i < CurrentWaveRow->SpawnCount; i++)
+	{
+		if (CurrentSpawnCount >= CurrentWaveRow->MaxSpawnCount)
+		{
+			break;
+		}
+		ADrgBaseCharacter* SpawnedAI = SpawnAIFromPool();
+		if (SpawnedAI)
+		{
+			ActiveAIPool.Add(SpawnedAI);
+			CurrentSpawnCount++;
+		}
+	}
+	// 웨이브 넘버 연결 전에 테스트용
+	if (CurrentSpawnCount >= CurrentWaveRow->MaxSpawnCount)
+	{
+		StopSpawnTimer();
+		CurrentSpawnCount = 0;
+		CurrentWaveNumber++;
+		if (CurrentWaveNumber < 3)
+		{
+			StartSpawnTimer(CurrentWaveNumber);
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning,
+			       TEXT("DrgSpawnAI::웨이브 종료"));
+		}
 	}
 }
