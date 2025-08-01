6dce7e372ed4e20daf3433846637cd35d018b0e4
SKim041 <sehee1806@gmail.com>
2025-08-01 16:54:46 +0900

    feat(AI): DrgAnimNotify_SendGameplayEvent 추가
    DrgAnimNotify_SendGameplayEvent: 애님 몽타주에 추가하여 사용. Event Tag를 보내 Gameplay Event 실행

---
+++ Source/Drg/AbilitySystem/AnimNotifies/DrgAnimNotify_SendGameplayEvent.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "GameplayTagContainer.h"
+#include "DrgAnimNotify_SendGameplayEvent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRG_API UDrgAnimNotify_SendGameplayEvent : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	                    const FAnimNotifyEventReference& EventReference) override;
+	// 디자이너가 에디터에서 설정할 이벤트 태그
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GameplayEvent")
+	FGameplayTag EventTag;
+};
