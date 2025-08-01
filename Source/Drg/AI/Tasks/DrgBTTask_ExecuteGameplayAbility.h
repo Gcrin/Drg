d436507dc935cad96d266f847f75ab4e0348266e
SKim041 <sehee1806@gmail.com>
2025-08-01 15:28:16 +0900

    feat(AI):DrgBTTask_ExecuteGameplayAbility 추가
    Tag로 해당 어빌리티 실행

---
+++ Source/Drg/AI/Tasks/DrgBTTask_ExecuteGameplayAbility.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "GameplayTagContainer.h"
+#include "Abilities/GameplayAbilityTypes.h"
+#include "DrgBTTask_ExecuteGameplayAbility.generated.h"
+struct FBTExecuteGameplayAbilityMemory
+{
+	TWeakObjectPtr<UAbilitySystemComponent> ASC;
+	FDelegateHandle OnAbilityEndedHandle;
+	bool bIsAbilityActive = false;
+	UBehaviorTreeComponent* OwnerComp = nullptr; 
+};
+
+UCLASS()
+class DRG_API UDrgBTTask_ExecuteGameplayAbility : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UDrgBTTask_ExecuteGameplayAbility();
+
+	// BT 에디터에서 디자이너가 설정할 어빌리티 태그
+	UPROPERTY(EditAnywhere, Category = "GameplayAbility")
+	FGameplayTag AbilityTag;
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual EBTNodeResult::Type AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual void OnTaskFinished(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, EBTNodeResult::Type TaskResult) override;
+	virtual uint16 GetInstanceMemorySize() const override;
+
+private:
+	void OnAbilityEnded(const FAbilityEndedData& EndedData);
+	void Cleanup(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory);
+
+	uint8* CachedNodeMemory = nullptr;
+};
