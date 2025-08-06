45089b44752f5acde852204ebd42af1f9f57323f
SKim041 <sehee1806@gmail.com>
2025-08-06 20:09:02 +0900

    feat(Core): DrgGA_Death C++ 구현
    - GA_Drg_Death.uasset 부모클래스로 DrgGA_Death 구현
    - GetOwnerData(): OwnerActor의 캐릭터, 어빌리티시스템컴포넌트, 애님몽타주 구조체에 넣어 출력
    - EndAbility(): Super::EndAbility 이후 캐릭터 클래스의 OnDeathCleanup() 실행

---
+++ Source/Drg/AbilitySystem/Abilities/DrgGA_Death.cpp
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DrgGA_Death.h"
+#include "Drg/System/DrgGameplayTags.h"
+#include "Drg/Character/DrgBaseCharacter.h"
+#include "AbilitySystemComponent.h"
+#include "Drg/Character/Data/DrgCharacterData.h"
+
+UDrgGA_Death::UDrgGA_Death()
+{
+	FGameplayTagContainer AssetTagContainer;
+	AssetTagContainer.AddTag(DrgGameplayTags::Event_Death);
+	SetAssetTags(AssetTagContainer);
+}
+
+void UDrgGA_Death::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+                              const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility,
+                              bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+
+	ADrgBaseCharacter* OwnerCharacter = CastChecked<ADrgBaseCharacter>(ActorInfo->OwnerActor);
+	if (OwnerCharacter)
+	{
+		OwnerCharacter->OnDeathCleanup();
+	}
+}
+
+FOwnerData UDrgGA_Death::GetOwnerData() const
+{
+	FOwnerData OwnerData;
+	const AActor* OwnerActor = GetOwningActorFromActorInfo();
+	if (OwnerActor)
+	{
+		OwnerData.OwningCharacter = CastChecked<ADrgBaseCharacter>(OwnerActor);
+		OwnerData.AbilitySystemComponent = OwnerData.OwningCharacter->GetAbilitySystemComponent();
+		auto CharacterData = OwnerData.OwningCharacter->GetCharacterData();
+		if (CharacterData)
+		{
+			TObjectPtr<UAnimMontage>* FoundMontage = CharacterData->AbilityMontages.Find(CharacterData->DeathTypeTag);
+			if (FoundMontage)
+			{
+				OwnerData.DeathMontage = *FoundMontage;
+			}
+			else
+			{
+				UE_LOG(LogTemp, Warning, TEXT("해당 태그에 해당하는 몽타주를 찾지 못했습니다 : %s"),
+				       *CharacterData->DeathTypeTag.ToString());
+			}
+		}
+	}
+	return OwnerData;
+}
---
+++ Source/Drg/AbilitySystem/Abilities/DrgGA_Death.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DrgGameplayAbility.h"
+#include "DrgGA_Death.generated.h"
+
+class UAnimMontage;
+class UGameplayEffect;
+class ADrgBaseCharacter;
+
+USTRUCT(BlueprintType)
+struct FOwnerData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<const ADrgBaseCharacter> OwningCharacter = nullptr;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent = nullptr;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UAnimMontage> DeathMontage = nullptr;
+};
+
+UCLASS()
+class DRG_API UDrgGA_Death : public UDrgGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UDrgGA_Death();
+
+protected:
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	                        const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility,
+	                        bool bWasCancelled) override;
+	UFUNCTION(BlueprintPure, Category = "Drg|Ability")
+	FOwnerData GetOwnerData() const;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Drg|GAS")
+	TSubclassOf<UGameplayEffect> DeathEffect;
+};
