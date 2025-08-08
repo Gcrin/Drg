1849e28f377c211da368bde8f113f26e66a4ee1b
Gcrin <94522022+Gcrin@users.noreply.github.com>
2025-08-08 11:15:17 +0900

    feat(GameModes): 게임 상태 관리 서브시스템에 비동기 맵 로딩 기능 추가
    맵 전환 시 데이터 에셋을 비동기로 로드하여 성능과 관리성을 개선.
    
    - `UDrgGameStateManagerSubsystem`에 `StartLoadingSequence` 및 `OnMapDataLoaded` 함수 추가
    - `MapDataAssetPath` 경로를 통해 `DA_MapData` 데이터 에셋 비동기 로드 지원
    - 각 게임 상태에 따른 맵 로딩 로직 개선 (`OpenLevelBySoftObjectPtr` 활용)
    - 데이터 에셋 경로 및 기본 맵을 변경하도록 `DefaultGame.ini` 및 `DefaultEngine.ini` 수정
    - 맵 데이터 관리 및 전환 제어를 위한 사전 데이터 로드 방식 구현

--- Source/Drg/GameModes/DrgGameStateManagerSubsystem.cpp
+++ Source/Drg/GameModes/DrgGameStateManagerSubsystem.cpp
@@ -3,14 +3,42 @@
 
 #include "DrgGameStateManagerSubsystem.h"
 
+#include "Drg/Maps/Data/DrgMapDataAsset.h"
+#include "Engine/AssetManager.h"
 #include "Kismet/GameplayStatics.h"
 
 
 void UDrgGameStateManagerSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
 	Super::Initialize(Collection);
+}
 
-	ChangeState(EGameFlowState::MainMenu);
+void UDrgGameStateManagerSubsystem::StartLoadingSequence()
+{
+	checkf(MapDataAssetPath.IsValid(),
+	       TEXT("UDrgGameStateManagerSubsystem 설정에 MapDataAssetPath가 지정되지 않았습니다. DefaultGame.ini 파일을 확인해주세요."));
+
+	UWorld* CurrentWorld = GetWorld();
+	if (!ensure(CurrentWorld)) return;
+
+	// 로딩 화면 맵으로 먼저 이동
+	UGameplayStatics::OpenLevel(CurrentWorld, TEXT("L_LoadingMap"));
+
+	// 백그라운드에서 실제 데이터 에셋 로딩 요청
+	FStreamableManager& StreamableManager = UAssetManager::Get().GetStreamableManager();
+	StreamableManager.RequestAsyncLoad(
+		MapDataAssetPath.ToSoftObjectPath(),
+		FStreamableDelegate::CreateUObject(this, &UDrgGameStateManagerSubsystem::OnMapDataLoaded)
+	);
+}
+
+void UDrgGameStateManagerSubsystem::OnMapDataLoaded()
+{
+	LoadedMapDataAsset = MapDataAssetPath.Get();
+	if (ensure(LoadedMapDataAsset))
+	{
+		ChangeState(EGameFlowState::MainMenu);
+	}
 }
 
 void UDrgGameStateManagerSubsystem::ChangeState(EGameFlowState NewState)
@@ -21,35 +49,39 @@ void UDrgGameStateManagerSubsystem::ChangeState(EGameFlowState NewState)
 		return;
 	}
 
+	checkf(LoadedMapDataAsset, TEXT("LoadedMapDataAsset가 유효하지 않습니다. OnMapDataLoaded()가 호출되지 않았거나 실패했습니다."));
+
 	CurrentState = NewState;
 
 	switch (CurrentState)
 	{
-		case EGameFlowState::MainMenu:
-			HandleMainMenuState();
-			break;
-		case EGameFlowState::InGame:
-			HandleInGameState();
-			break;
-		case EGameFlowState::PostGame:
-			HandlePostGameState();
-			break;
-		case EGameFlowState::Quitting:
-			HandleQuittingState();
-			break;
+	case EGameFlowState::MainMenu:
+		HandleMainMenuState();
+		break;
+	case EGameFlowState::InGame:
+		HandleInGameState();
+		break;
+	case EGameFlowState::PostGame:
+		HandlePostGameState();
+		break;
+	case EGameFlowState::Quitting:
+		HandleQuittingState();
+		break;
+	default:
+		break;
 	}
 }
 
 void UDrgGameStateManagerSubsystem::HandleMainMenuState()
 {
 	UE_LOG(LogTemp, Display, TEXT("메인 메뉴로 이동"));
-	UGameplayStatics::OpenLevel(this, FName("L_MainMenu"));
+	UGameplayStatics::OpenLevelBySoftObjectPtr(this, LoadedMapDataAsset->MainMenuMap);
 }
 
 void UDrgGameStateManagerSubsystem::HandleInGameState()
 {
 	UE_LOG(LogTemp, Display, TEXT("게임 레벨로 이동"));
-	UGameplayStatics::OpenLevel(this, FName("L_TestMap"));
+	UGameplayStatics::OpenLevelBySoftObjectPtr(this, LoadedMapDataAsset->InGameMap);
 }
 
 void UDrgGameStateManagerSubsystem::HandlePostGameState()
--- Source/Drg/GameModes/DrgGameStateManagerSubsystem.h
+++ Source/Drg/GameModes/DrgGameStateManagerSubsystem.h
@@ -6,6 +6,8 @@
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "DrgGameStateManagerSubsystem.generated.h"
 
+class UDrgMapDataAsset;
+
 UENUM(BlueprintType)
 enum class EGameFlowState : uint8
 {
@@ -21,7 +23,7 @@ enum class EGameFlowState : uint8
 	Quitting
 };
 
-UCLASS()
+UCLASS(Config = Game)
 class DRG_API UDrgGameStateManagerSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
@@ -35,11 +37,31 @@ public:
 
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
 
+	// 게임모드 등 외부에서 호출할 로딩 시퀀스 시작 함수
+	void StartLoadingSequence();
+
+protected:
+	// 게임 흐름에 필요한 맵 정보가 담긴 데이터 에셋의 경로입니다.
+	UPROPERTY(Config)
+	TSoftObjectPtr<UDrgMapDataAsset> MapDataAssetPath;
+
 private:
 	void HandleMainMenuState();
 	void HandleInGameState();
 	void HandlePostGameState();
 	void HandleQuittingState();
 
+	// 데이터 로딩이 완료되면 호출되는 함수
+	void OnMapDataLoaded();
+
 	EGameFlowState CurrentState = EGameFlowState::None;
+
+	/**
+	 * MapDataAssetPath를 통해 비동기로 로드된 맵 데이터 에셋의 const 포인터입니다.
+	 *
+	 * OnMapDataLoaded() 콜백 함수에서 유효한 값을 가지게 되며,
+	 * 이후 각 게임 상태에 맞는 맵으로 이동할 때 사용됩니다.
+	 */
+	UPROPERTY()
+	TObjectPtr<const UDrgMapDataAsset> LoadedMapDataAsset;
 };
