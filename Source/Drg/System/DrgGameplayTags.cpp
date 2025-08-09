f2c78666834c41d111b9d37faefdc64523431b5a
Gcrin <94522022+Gcrin@users.noreply.github.com>
2025-08-10 02:35:21 +0900

    feat(System): Ability.Multiplier 게임플레이 태그 추가
    어빌리티 배율 관리를 위한 새로운 게임플레이 태그 `Ability.Multiplier` 를 정의하고, 관련 헤더 및 CPP 파일에 선언을 추가.
    
    - 신규 태그 `Ability.Multiplier`: 어빌리티 배율 값을 나타냄
    - `DrgGameplayTags.h` 및 `DrgGameplayTags.cpp` 업데이트

--- Source/Drg/System/DrgGameplayTags.cpp
+++ Source/Drg/System/DrgGameplayTags.cpp
@@ -7,4 +7,5 @@ namespace DrgGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Dead, "State.Dead", "캐릭터가 죽은 상태를 나타냅니다.");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Event_Death, "Event.Death",
 	                               "죽음 이벤트를 브로드캐스트합니다. 이 태그를 수신 대기하는 시스템(주로 Gameplay Ability)이 죽음 관련 로직을 실행합니다.");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_Multiplier, "Ability.Multiplier", "어빌리티 배율입니다.");
 }
